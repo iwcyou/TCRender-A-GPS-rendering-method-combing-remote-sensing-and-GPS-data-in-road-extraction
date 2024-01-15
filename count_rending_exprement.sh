0cb29482fdb96a49bbd15a1670f1ec3542e6009b
fk <465448286@qq.com>
2024-01-15 14:37:39 +0000

    0.0

--- count_rending_exprement.sh
+++ count_rending_exprement.sh
@@ -2,19 +2,19 @@
 #dlink34模型
 
 
-#sat+GPSdata,plain
-ts -G 1 python train.py \
-    --model dlink34 \
-    --sat_dir datasets/dataset_time/train_val/image \
-    --mask_dir datasets/dataset_time/train_val/mask \
-    --test_sat_dir datasets/dataset_time/test/image_test \
-    --test_mask_dir datasets/dataset_time/test/mask \
-    --gps_dir datasets/dataset_time/GPS/patch \
-    \
-    --gps_type data \
-    --gps_render_type count \
-    --count_render_type direct \
-    --epochs 60
+# #sat+GPSdata,plain
+# ts -G 1 python train.py \
+#     --model dlink34 \
+#     --sat_dir datasets/dataset_time/train_val/image \
+#     --mask_dir datasets/dataset_time/train_val/mask \
+#     --test_sat_dir datasets/dataset_time/test/image_test \
+#     --test_mask_dir datasets/dataset_time/test/mask \
+#     --gps_dir datasets/dataset_time/GPS/patch \
+#     \
+#     --gps_type data \
+#     --gps_render_type count \
+#     --count_render_type direct \
+#     --epochs 60
 
 #sat+GPSdata,plain
 ts -G 1 python train.py \
--- trainmine.sh
+++ trainmine.sh
@@ -18,3 +18,48 @@
 #     --test_mask_dir datasets/dataset_mask7/test/mask \
 #     --gps_dir datasets/dataset_mask7/GPS/taxi_time_count_patch \
 #     --gps_type image
+
+
+
+lora_hparams=(
+    "--lora_r=8 --lora_alpha=8"
+    "--lora_r=16 --lora_alpha=8"
+)
+sparse_hparams=(
+    "--sparse_k=0.04"
+    "--sparse_k=0.16"
+)
+tasks=(
+    "cola"
+    "mnli"
+    "mrpc"
+    "qnli"
+    "qqp"
+    "rte"
+    "sst2"
+    "stsb"
+    "wnli"
+)
+sparse_methods=(
+    "magnitude"
+    "gradient"
+    "grasp"
+    "synflow"
+)
+for t in "${tasks[@]}"; do
+    echo "Queueing ${t}..."
+    for h in "${lora_hparams[@]}"; do
+        echo ts -G 1 python cls_train.py \
+            --lora_enable ${h} \
+            --per_device_train_batch_size=32 \
+            --task_name="${t}"
+    done
+    for h in "${sparse_hparams[@]}"; do
+        for s in "${sparse_methods[@]}"; do
+            echo ts -G 1 python cls_train.py \
+                --sparse_enable --sparse_method="${s}" ${h} \
+                --per_device_train_batch_size=32 \
+                --task_name="${t}"
+        done
+    done
+done

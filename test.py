128adc0c66697203d9ecdb1614dace7c32ec07e8
fk <465448286@qq.com>
2024-03-22 14:05:19 +0000

    calculate 1D emd for images

--- test.py
+++ test.py
@@ -1,27 +1,46 @@
-import cv2
 import os
-import sys
-# sys.path.append('..')
+from scipy.stats import wasserstein_distance
+import cv2
+import numpy as np
+import csv
+from tqdm import tqdm
 
-def transpose_image(input_path, output_path):
-    # Read the input image
-    img = cv2.imread(input_path)
+weights_dirs = ["weights_sz_v1", "weights_sz_v2", "weights_sz_v3"]
+models = ["unet", "resunet", "deeplabv3+", "linknet", "dlink34_1d", "dlink34"]
+methods = ["_gpsimage_only_gaussian_ltqs_log__", "_sat_gpsimage_gaussian_ltqs_log__"]
 
-    # Resize the image to 128x128 while maintaining aspect ratio
-    resized_img = cv2.resize(img, (128, 128), interpolation=cv2.INTER_AREA)
+mask_path = "./datasets/dataset_sz_grid/test/mask"
+csv_file_path = "./emd.csv"
 
-    # Save the resized image
-    cv2.imwrite(output_path, resized_img)
+for weights_dir in weights_dirs:
+    print(f"Calculating Wasserstein distance for {weights_dir}:\n")
+    for model in models:
+        print(f"Model: {model}\n")
+        for method in methods:
+            print(f"Method: {method}\n")
+            folder_path = f"./{weights_dir}/{model}{method}"
+            pred_path = os.path.join(folder_path, "prediction")
+            sum_distance = 0
+            for filename in tqdm(os.listdir(pred_path)):
+                if filename.endswith(".png"):
+                    pred_image = cv2.imread(os.path.join(pred_path, filename), cv2.IMREAD_GRAYSCALE)
+                    mask_image = cv2.imread(os.path.join(mask_path, f"{filename[:-4]}_mask.png"), cv2.IMREAD_GRAYSCALE)
+                    distance = np.zeros(pred_image.shape[0])
+                    for i, row in enumerate(pred_image):
+                        if row.sum() == 0 and mask_image[i].sum() == 0:
+                            distance[i] = 0
+                        elif row.sum() == 0 and mask_image[i].sum() != 0:
+                            distance[i] = wasserstein_distance([0], np.arange(1024), None, mask_image[i])
+                        elif row.sum() != 0 and mask_image[i].sum() == 0:
+                            distance[i] = wasserstein_distance(np.arange(1024), [0], row, None)
+                        else:
+                            # distance[i] = wasserstein_distance(np.arange(row.shape[0]), np.arange(mask_image[i].shape[0]), row, mask_image[i])
+                            distance[i] = wasserstein_distance(np.arange(1024), np.arange(1024), row, mask_image[i])
+                    sum_distance += np.mean(distance)
+            average_distance = sum_distance / len(os.listdir(pred_path))
+            # Write average_distance to CSV file
+            with open(csv_file_path, mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([folder_path, average_distance])
 
-# Example usage:
-path = "weights_sz_grid/dlink34_sat_gpsdata_count_direct__/prediction"
-save_path = ""
-for filename in os.listdir(path):
-    if filename.endswith(".png"):
-        input_image_path = os.path.join(path, filename)
-        output_image_path = os.path.join(path, filename)
-        transpose_image(input_image_path, output_image_path)
-        print(f'Transposed: {input_image_path} -> {output_image_path}')
-    break
-
-print("Done!")
+            print(f'{folder_path}: {average_distance}')

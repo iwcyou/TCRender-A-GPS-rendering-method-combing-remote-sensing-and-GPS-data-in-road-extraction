df44dcdfd9df06577e6d7645d5d9d6ee29e4bb40
fk <465448286@qq.com>
2024-03-25 09:07:22 +0000

    add transpose pred images size code

--- tr_test.sh
+++ tr_test.sh
@@ -1,16 +1,15 @@
 #测试用的脚本
 
-# ltqs, GPS
-ts -G 2 python train.py \
+ts -G 1 python train.py \
+    --model unet \
+    --weight_save_dir ./weights_sz_v3_add \
     --dataset_name sz \
-    --model resunet \
-    --weight_save_dir ./weights_sz_v3 \
-    --sat_dir '' \
+    --sat_dir datasets/dataset_sz_grid/train_val/image \
     --mask_dir datasets/dataset_sz_grid/train_val/mask \
-    --test_sat_dir '' \
+    --test_sat_dir datasets/dataset_sz_grid/test/image_test \
     --test_mask_dir datasets/dataset_sz_grid/test/mask \
-    --gps_dir datasets/dataset_sz_grid/GPS/taxi_gaussian_ltqs_patch \
+    --gps_dir datasets/dataset_sz_grid/GPS/taxi \
     \
-    --gps_type image \
-    --gps_render_type gaussian_ltqs \
-    --count_render_type log
+    --gps_type data \
+    --gps_render_type count \
+    --count_render_type direct

e116a24dee54054e5a32e98279148d46b46e59d6
g4 <465448286@qq.com>
2024-07-04 09:06:19 +0000

    run abalation experiment

--- tr_ablation.sh
+++ tr_ablation.sh
@@ -1,49 +1,82 @@
-"""
-训练sun的数据集
-使用DlinkNet模型
-消融实验
-"""
-
-# #tcrender只渲染数据的时间信息
-# ts -G 1 python train.py \
-#     --model dlink34 \
-#     --sat_dir datasets/dataset_time/train_val/image \
-#     --mask_dir datasets/dataset_time/train_val/mask \
-#     --test_sat_dir datasets/dataset_time/test/image_test \
-#     --test_mask_dir datasets/dataset_time/test/mask \
-#     --gps_dir datasets/dataset_time/GPS/time_patch \
-#     \
-#     --gps_type image \
-#     --gps_render_type time \
-#     --count_render_type direct \
-#     --epochs 60
+# train bj dataset
+# use DlinkNet model
+# ablation experiment
 
 
-# #tcrender只渲染数据的速度信息
-# ts -G 1 python train.py \
-#     --model dlink34 \
-#     --sat_dir datasets/dataset_time/train_val/image \
-#     --mask_dir datasets/dataset_time/train_val/mask \
-#     --test_sat_dir datasets/dataset_time/test/image_test \
-#     --test_mask_dir datasets/dataset_time/test/mask \
-#     --gps_dir datasets/dataset_time/GPS/speed_patch \
-#     \
-#     --gps_type image \
-#     --gps_render_type speed \
-#     --count_render_type direct \
-#     --epochs 60
-
-
-#tcrender只渲染数据的时间和数量信息
-ts -G 1 python train.py \
-    --model dlink34 \
-    --sat_dir datasets/dataset_time/train_val/image \
-    --mask_dir datasets/dataset_time/train_val/mask \
-    --test_sat_dir datasets/dataset_time/test/image_test \
-    --test_mask_dir datasets/dataset_time/test/mask \
-    --gps_dir datasets/dataset_time/GPS/time_quantity_patch \
-    \
-    --gps_type image \
-    --gps_render_type time_quantity \
-    --count_render_type log \
-    --epochs 60
+weight_save_dir=(
+    "./weights_sz_v1"
+    "./weights_sz_v2"
+    # "./weights_sz_v3"
+)
+models=(
+    "dlink34"
+)
+for w in "${weight_save_dir[@]}"; do
+    for m in "${models[@]}"; do
+        if [ "${m}" == "resunet" ]; then
+            n=2
+        else
+            n=1
+        fi
+        # only embedding temporal information
+        ts -G "${n}" python train.py \
+            --model "${m}" \
+            --weight_save_dir "${w}" \
+            --dataset_name bj \
+            --sat_dir datasets/dataset_bj_time/train_val/image \
+            --mask_dir datasets/dataset_bj_time/train_val/mask \
+            --test_sat_dir datasets/dataset_bj_time/test/image_test \
+            --test_mask_dir datasets/dataset_bj_time/test/mask \
+            --gps_dir datasets/dataset_bj_time/GPS/time_patch \
+            \
+            --gps_type image \
+            --gps_render_type time \
+            --count_render_type direct \
+            --epochs 60
+        # only embedding rate-of-occurrence information
+        ts -G "${n}" python train.py \
+            --model "${m}" \
+            --weight_save_dir "${w}" \
+            --dataset_name bj \
+            --sat_dir datasets/dataset_bj_time/train_val/image \
+            --mask_dir datasets/dataset_bj_time/train_val/mask \
+            --test_sat_dir datasets/dataset_bj_time/test/image_test \
+            --test_mask_dir datasets/dataset_bj_time/test/mask \
+            --gps_dir datasets/dataset_bj_time/GPS/gaussian_patch \
+            \
+            --gps_type image \
+            --gps_render_type quantity \
+            --count_render_type direct \
+            --epochs 60
+        # only embedding speed information
+        ts -G "${n}" python train.py \
+            --model "${m}" \
+            --weight_save_dir "${w}" \
+            --dataset_name bj \
+            --sat_dir datasets/dataset_bj_time/train_val/image \
+            --mask_dir datasets/dataset_bj_time/train_val/mask \
+            --test_sat_dir datasets/dataset_bj_time/test/image_test \
+            --test_mask_dir datasets/dataset_bj_time/test/mask \
+            --gps_dir datasets/dataset_bj_time/GPS/speed_patch \
+            \
+            --gps_type image \
+            --gps_render_type speed \
+            --count_render_type direct \
+            --epochs 60
+        # only embedding time and quantity information
+        ts -G "${n}" python train.py \
+            --model "${m}" \
+            --weight_save_dir "${w}" \
+            --dataset_name bj \
+            --sat_dir datasets/dataset_bj_time/train_val/image \
+            --mask_dir datasets/dataset_bj_time/train_val/mask \
+            --test_sat_dir datasets/dataset_bj_time/test/image_test \
+            --test_mask_dir datasets/dataset_bj_time/test/mask \
+            --gps_dir datasets/dataset_bj_time/GPS/time_quantity_patch \
+            \
+            --gps_type image \
+            --gps_render_type time_quantity \
+            --count_render_type log \
+            --epochs 60
+    done
+done
--- utils/metrics.py
+++ utils/metrics.py
@@ -17,23 +17,24 @@ class IoU(nn.Module):
         intersection = torch.clamp(input_ * target_, 0, 1)
         union = torch.clamp(input_ + target_, 0, 1)
 
-        #EMD
-        # Calculate the sum of all pixel values
-        sum_of_target = np.sum(target.data.float().cpu().numpy())
-        sum_of_input = np.sum(input.data.float().cpu().numpy())
-        # Normalize pixel values
-        normalized_target = target.data.float().cpu().numpy() / sum_of_target
-        normalized_input = input.data.float().cpu().numpy() / sum_of_input
-        # Optionally, you can convert the data type to ensure it's in floating point
-        normalized_target = normalized_target.astype('float32')
-        normalized_input = normalized_input.astype('float32')
+        # #EMD
+        # # Calculate the sum of all pixel values
+        # sum_of_target = np.sum(target.data.float().cpu().numpy())
+        # sum_of_input = np.sum(input.data.float().cpu().numpy())
+        # # Normalize pixel values
+        # normalized_target = target.data.float().cpu().numpy() / sum_of_target
+        # normalized_input = input.data.float().cpu().numpy() / sum_of_input
+        # # Optionally, you can convert the data type to ensure it's in floating point
+        # normalized_target = normalized_target.astype('float32')
+        # normalized_input = normalized_input.astype('float32')
 
-        emd = np.zeros(len(normalized_target))
-        for i in range(len(normalized_target)): # every batch calculate respectively
-            real_target = normalized_target[i].flatten()
-            predicted_mask = normalized_input[i].flatten()
-            emd[i] = wasserstein_distance(real_target, predicted_mask)
-        emd = np.mean(emd)
+        # emd = np.zeros(len(normalized_target))
+        # for i in range(len(normalized_target)): # every batch calculate respectively
+        #     real_target = normalized_target[i].flatten()
+        #     predicted_mask = normalized_input[i].flatten()
+        #     emd[i] = wasserstein_distance(real_target, predicted_mask)
+        # emd = np.mean(emd)
+        emd = 0
 
         if torch.mean(intersection).lt(eps): #less than函数
             return torch.Tensor([0., 0., 0., 0., 0., emd])
@@ -64,5 +65,5 @@ class IoU(nn.Module):
     #     predicted_mask = normalized_input.flatten()
     #     emd = wasserstein_distance(real_target, predicted_mask)
 
-    #     # return torch.Tensor([acc, recall, precision, iou, f1, emd])
-    #     return torch.Tensor([0., 0., 0., 0., 0., emd])
+    #     return torch.Tensor([acc, recall, precision, iou, f1, emd])
+    #     # return torch.Tensor([0., 0., 0., 0., 0., emd])
